1b8594741973d69a122a6d4110926dfcf7e9eedb
coolfire02 - laptop <coolfire02@vortexnetwork.me>
2021-01-27 16:32:34 +0800

    alex more like aluxitsm autism

--- Yong Hong is gay/Source.cpp
+++ Yong Hong is gay/Source.cpp
@@ -7,6 +7,7 @@ int main(void)
 	cout << "Hello World, I'm Marie, potatoe\n";
 	cout << "Hello World, I'm Alex, dumbass" << endl;
 	cout << "Hello World! I'm Jia Lung" << endl;
+	cout << "Hello World! fuck off alex" << endl;
 
 	return 0;
 }
