b44a0b52e320cfc086f8a712fcdc4f730f74ef07
Koh Jia Lung <jialungkoh@gmail.com>
2021-01-27 16:52:18 +0800

    test

--- Yong Hong is gay/Source.cpp
+++ Yong Hong is gay/Source.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -8,6 +9,8 @@ int main(void)
 	cout << "Hello World, I'm Alex, dumbass" << endl;
 	cout << "Hello World! I'm Jia Lung" << endl;
 	cout << "Hello World! fuck off alex" << endl;
-
+	cout << "Press enter" << endl;
+	cin.ignore();
+	system("start https://www.youtube.com/watch?v=oHg5SJYRHA0");
 	return 0;
 }
