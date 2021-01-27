e8113304188263b1d48350a358974bb3a29d1f66
PikaChiMathers <PikaChiMathers@users.noreply.github.com>
2021-01-27 16:47:34 +0800

    Added a quote selection and reader???

--- Yong Hong is gay/Source.cpp
+++ Yong Hong is gay/Source.cpp
@@ -2,12 +2,53 @@
 
 using namespace std;
 
+
+
+enum QUOTES
+{
+	QUIT = 0,
+	ALEX,
+	MARIE,
+	JIA_LUNG,
+	YONG_HONG,
+	TOTAL_QUOTES
+};
+
 int main(void)
 {
-	cout << "Hello World, I'm Marie, potatoe\n";
-	cout << "Hello World, I'm Alex, dumbass" << endl;
-	cout << "Hello World! I'm Jia Lung" << endl;
-	cout << "Hello World! fuck off alex" << endl;
+	bool reading = true;
+	while (reading)
+	{
+		int quote;
 
+		cout << "QUOTES:\
+1) Alex\n\
+2) Marie\n\
+3) Jia Lung\n\
+4) Yong Hong\n\
+Narrator: Whose quote would you like to see?: "; cin >> quote;
+
+		switch (quote)
+		{
+		case(QUIT):
+			reading = false;
+			break;
+		case(MARIE):
+			cout << "Marie: Hello World, I'm Marie, potatoe\n\n";
+			break;
+		case(ALEX):
+			cout << "Alex: Hello World, I'm Alex, dumbass\n" << endl;
+			break;
+		case(JIA_LUNG):
+			cout << "Jia Lung: Hello World! I'm Jia Lung\n" << endl;
+			break;
+		case(YONG_HONG):
+			cout << "Yong Hong: Hello World! fuck off alex\n" << endl;
+			break;
+		default:
+			cout << "Narrator: Dude choose a proper Quote No.\n\n";
+			break;
+		}
+	}
 	return 0;
 }
