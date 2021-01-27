60e255dc7200d9bf35f95e2aa82528a6b4a45c67
coolfire02 - laptop <coolfire02@vortexnetwork.me>
2021-01-27 16:50:57 +0800

    d

--- Yong Hong is gay/Source.cpp
+++ Yong Hong is gay/Source.cpp
@@ -2,12 +2,43 @@
 
 using namespace std;
 
+
+void A() {
+	B();
+}
+
+void B() {
+	C();
+}
+
+void C() {
+	D();
+}
+
+void D() {
+	E();
+}
+
+void E() {
+	F();
+}
+
+void F() {
+	B();
+	for(int i = 0; i < 100000; i++) {
+		cout << "Alex Loves renee" << endl;
+	}
+}
+
 int main(void)
 {
 	cout << "Hello World, I'm Marie, potatoe\n";
 	cout << "Hello World, I'm Alex, dumbass" << endl;
 	cout << "Hello World! I'm Jia Lung" << endl;
 	cout << "Hello World! fuck off alex" << endl;
+
+
+	A();
 
 	return 0;
 }
